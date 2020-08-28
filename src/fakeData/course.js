1a285e51a2f3e06be3302e890d2db0642ba53f7b
MuhammadSaidulislam <61590810+MuhammadSaidulislam@users.noreply.github.com>
2020-08-28 21:57:47 +0600

    Add files via upload

---
+++ src/fakeData/course.js
@@ -0,0 +1,65 @@
+var courses =
+    [
+        {
+        "name":'Computer Graphics',
+        "price":'10000',
+        "img":"https://i1.wp.com/www.bapugraphics.com/blog/wp-content/uploads/2015/11/Ways-to-Learn-Graphic-Designing-Course-by-Graphics-Design-Institute-in-Delhi1.jpg?ssl=1"
+    },
+    {
+        "name":'Web Design',
+        "price":'15000',
+        "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSDU4K7-1VR5bNz0x5LRAJgc0huLkTQEKV8wA&usqp=CAU"
+    },
+    {
+        "name":'Web Developer',
+        "price":'20000',
+        "img":"https://cdn-5ce4a003f911c80f50818892.closte.com/wp-content/uploads/2019/07/become-web-developer-768x512.jpg"
+    },
+    {
+        "name":'Animation Designer',
+        "price":'22000',
+        "img":"https://www.pulsecollege.com/wp-content/uploads/game-designer-versus-game-developer-pulse-college-dublin-1024x536.png"
+    },
+    {
+        "name":'Mobile App Developer',
+        "price":'35000',
+        "img":"https://i.ytimg.com/vi/d8qx42mhGCg/maxresdefault.jpg"
+    },
+    {
+        "name":'Wordpress Developer',
+        "price":'8000',
+        "img":"https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/122563851/original/d3d304a65dfe04661d167358898900928a8893c8/be-your-wordpress-developer-08fe.jpg"
+    },
+    {
+        "name":'Full-Stack Web developer',
+        "price":'30000',
+        "img":"https://i.pinimg.com/474x/be/c3/2f/bec32ffc65cf051af7bf2c71a9af424d.jpg"
+    },
+    {
+        "name":'Upwork Success',
+        "price":'12000',
+        "img":"https://mostaql.hsoubcdn.com/uploads/433343-GODyI-1533555542-The-Wolf-of-Upwork.png"
+    },
+    {
+        "name":'Laravel Developer',
+        "price":'16000',
+        "img":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS7tSZkFtznq-SKNs8n4cMdAb6XhTGdZFzTrA&usqp=CAU"
+    },
+    {
+        "name":'Fiverr Success',
+        "price":'10000',
+        "img":"https://img-a.udemycdn.com/course/750x422/2210800_66e1_3.jpg"
+    },
+    {
+        "name":'Django Developer',
+        "price":'20000',
+        "img":"https://elasticbeanstalk-ap-southeast-1-805366489044.s3.amazonaws.com/images/uploads/2019/03/30/python-django_QhCttHZ_TqhlDil_c3F1LmU.gif"
+    },
+    {
+        "name":'NOdeJS developer',
+        "price":'22000',
+        "img":"https://miro.medium.com/max/1000/1*Jx8EXy-c6XKnIEQgPGpz7Q.jpeg"
+    }
+];
+
+export default courses;
