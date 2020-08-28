9ad0cdc4c5cdf118de0d42a39ccad544f947d390
MuhammadSaidulislam <61590810+MuhammadSaidulislam@users.noreply.github.com>
2020-08-28 22:06:43 +0600

    Add files via upload

---
+++ build/precache-manifest.2a6f4d3ea43135472821d7fda17d85cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcebd0231b2fb055a80c567e2a0998d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beeb31730da6ad081cb2",
+    "url": "/static/css/main.dfd7fd99.chunk.css"
+  },
+  {
+    "revision": "8a0455254052a8fd991b",
+    "url": "/static/js/2.934a2d19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.934a2d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beeb31730da6ad081cb2",
+    "url": "/static/js/main.bf92c69a.chunk.js"
+  },
+  {
+    "revision": "60d21ee8588f46c03654",
+    "url": "/static/js/runtime-main.e64453f0.js"
+  }
+]);
