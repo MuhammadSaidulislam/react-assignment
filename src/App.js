6a274ef3f6e602d64f0c65656641ea6da8ea8364
MuhammadSaidulislam <61590810+MuhammadSaidulislam@users.noreply.github.com>
2020-08-28 21:54:21 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Shop from './components/shop/shop';
+ 
+ 
+
+function App() {
+  return (
+    <div>
+         <Shop></Shop>
+    </div>
+  );
+}
+ 
+export default App;
